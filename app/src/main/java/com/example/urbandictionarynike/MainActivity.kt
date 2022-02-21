cc014529e539258568c2d9e90bbfaaf98c51c256
Antonino AN <andonino@gmail.com>
2022-02-21 15:30:19 -0500

    Implemented menu elements

--- app/src/main/java/com/example/urbandictionarynike/MainActivity.kt
+++ app/src/main/java/com/example/urbandictionarynike/MainActivity.kt
@@ -2,15 +2,23 @@ package com.example.urbandictionarynike
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.appcompat.widget.SearchView
+import com.example.urbandictionarynike.di.DictionaryApplication
 import com.example.urbandictionarynike.model.DefinitionItem
 import com.example.urbandictionarynike.model.Repository
 import com.example.urbandictionarynike.model.RepositoryImpl
 import com.example.urbandictionarynike.model.UIState
+import com.example.urbandictionarynike.model.local.DictionaryRoom
 import com.example.urbandictionarynike.viewmodel.DictionaryProvider
 import com.example.urbandictionarynike.viewmodel.DictionaryViewModel
 import kotlinx.coroutines.*
 import javax.inject.Inject
 
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
 
     /**
@@ -21,7 +29,7 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var viewModelProvider: DictionaryProvider
 
-    private val viewModel by lazy{
+    private val viewModel by lazy {
         viewModelProvider.create(DictionaryViewModel::class.java)
     }
 
@@ -30,40 +38,83 @@ class MainActivity : AppCompatActivity() {
 //        repo
 //    )
     lateinit var job: Deferred<String>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        viewModel.definitions.observe(this){
-            when (it){
+// Because we use Field injection
+        DictionaryApplication.component.inject(this)
+
+
+        viewModel.definitions.observe(this) {
+            when (it) {
                 is UIState.Response -> updateAdapter(it.presentationData)
                 is UIState.Error -> showError(it.errorMessage)
                 is UIState.Loading -> displayLoading(it.isLoading)
                 is UIState.Online -> displayConnectivityBanner(it.isOnline)
             }
         }
-
     }
 
     private fun displayConnectivityBanner(online: Boolean) {
-
+        Log.d(TAG, "displayConnectivityBanner: $online")
     }
 
     private fun displayLoading(loading: Boolean) {
-
+        Log.d(TAG, "displayLoading: $loading")
     }
 
     private fun showError(errorMessage: String) {
-
+        Log.d(TAG, "showError: $errorMessage")
     }
 
     private fun updateAdapter(presentationData: List<DefinitionItem>) {
-
+        Log.d(TAG, "updateAdapter: $presentationData")
     }
 
     override fun onStop() {
         super.onStop()
         job.cancel()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.search_menu, menu)
+        val actionMenu = menu?.findItem(R.id.search_term)
+        (actionMenu?.actionView as SearchView)
+            .setOnQueryTextListener(
+                object : SearchView.OnQueryTextListener {
+                    override fun onQueryTextSubmit(query: String?): Boolean {
+                        query?.let {
+                            viewModel.findNewTerm(it)
+                        }
+                        return true
+                    }
+
+                    override fun onQueryTextChange(newText: String?): Boolean {
+                        return false
+                    }
+                }
+            )
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        super.onOptionsItemSelected(item)
+        return when (item.itemId) {
+            R.id.sort_down_menu -> {
+                viewModel.sort(DictionaryViewModel.SortingState.DOWN)
+                true
+            }
+            R.id.sort_up_menu -> {
+                viewModel.sort(DictionaryViewModel.SortingState.UP)
+                true
+            }
+            else -> false
+        }
+
     }
 }
 
@@ -82,3 +133,25 @@ suspend fun doo(): String {
 
     return ""
 }
+
+// Koin
+// Service Locator
+// object DictionaryKoin{
+//
+//    singleton{
+//        RepositoryImpl()
+//    }
+//    provide{
+//        DictionaryRoom.getDatabase().getDao()
+//    }
+//
+//    fun provideRoom(): Dao{
+//        return
+//    }
+//    fun provideRepository =
+//}
+
+
+
+
+
